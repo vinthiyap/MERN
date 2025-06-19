a8fa3500fb5240958b50211a4f66ad4d09c226cd
vinthiyap <vinthiyap.23ece@kongu.edu>
2025-06-19 16:06:58 +0530

    day3

---
+++ day3/vite-project/src/Welcome.jsx
@@ -0,0 +1,9 @@
+const  Welcome =(props) =>{
+    return(
+        <div>
+            <h1>Welcome{props.name}</h1>
+            <h2>Country:{props.country}</h2>
+            </div>
+    )
+}
+export default Welcome
---
+++ day3/vite-project/src/skills.jsx
@@ -0,0 +1,6 @@
+const skills = ({skill}) => {
+    return {
+        <div>
+        
+    }
+}
